8007e86a866fb59d061f343a0e0b27d496f5de27
Willian <willian9jr@hotmail>
2021-12-16 16:36:23 -0300

    Base criada

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # projeto-canil
+
+### Pré-reqiusitos globais:
+'npm i -g nodemon typescript ts-node'
+
+### Isntalação
+'npm install'
+
+### Para rodar o projeto
+'npm run start-dev'
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from 'express';
+
+export const home = (req: Request, res: Response) => {
+
+
+}
+
+export const dogs = (req: Request, res: Response) => {
+    //SDFS
+}
+
+export const cats = (req: Request, res: Response) => {
+
+
+}
+
+export const fishes = (req: Request, res: Response) => {
+
+
+}
